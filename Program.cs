445ec82b82e3b823da0c0e23d99015998e742397
SahibSodhi <mssodhi11@gmail.com>
2021-01-11 20:33:57 +0530

    Hello World in DotNet

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to MLH Local Hack: Build");
+            var name = "Sahibdeep";
+            Console.WriteLine($"I am {name}");
+        }
+    }
+}
